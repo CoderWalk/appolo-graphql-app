12c6fe8e11d199f362199039c966cc944ea4b368
rahul <rahul@gmail.com>
2026-03-09 22:19:52 +0530

    final-crud-user

--- src/graphql/resolvers/userResolver.js
+++ src/graphql/resolvers/userResolver.js
@@ -1,12 +1,42 @@
+const User = require("../../models/User");
 const userResolver = {
     Query: {
-        getUser: () => {
-            return { name: "Noman" };
+        users: async () => {
+            return await User.find().sort({ createdAt: -1 });
+        },
+        user: async (_, { id }) => {
+            return await User.findById(id);
         },
     },
     Mutation: {
-        createUser: (parent, args) => {
-            return { name: args.name };
+        createUser: async (_, { name, email }) => {
+            const exists = await User.findOne({ email });
+            if (exists) {
+                throw new Error("User already exists");
+            }
+            const user = await User.create({ name, email });
+            return user;
+            //const user = new User({ name, email });//new user instace obj is created
+            //return await user.save();
+        },
+        updateUser: async (_, { id, name, email }) => {
+            const update = {}
+            if (name !== undefined) {
+                update.name = name
+            }
+            if (email !== undefined) {
+                const exists = await User.findOne({ email });
+                if (exists) {
+                    throw new Error("User already exists");
+                }
+                update.email = email
+            }
+            const result = await User.findByIdAndUpdate(id, update, { new: true })
+            return result
+        },
+        deleteUser: async (_, { id }) => {
+            const result = await User.findByIdAndDelete(id);
+            return !!result;
         },
     },
 }
--- src/graphql/typedefs/userType.js
+++ src/graphql/typedefs/userType.js
@@ -2,15 +2,22 @@ const { gql } = require("apollo-server-express");
 
 const userType = gql`
     type User {
-        name: String!
+        id: ID!,
+        name: String!,
+        email: String!,
+        createdAt: String!,
+        updatedAt: String!
     }
 
     type Query {
-        getUser: User
+        users: [User],
+        user(id: ID!): User
     }
 
     type Mutation {
-        createUser(name: String!): User
+        createUser(name: String!, email: String!): User,
+        updateUser(id: ID!, name: String!, email: String!): User,
+        deleteUser(id: ID!): Boolean
     }
 `;
 
